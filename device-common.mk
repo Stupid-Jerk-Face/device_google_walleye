337cc81d067d926040916b57dc8a209c10d121c4
Christian Oder <myself5@carbonrom.org>
2018-01-26 22:29:52 +0100

    muskie/walleye: Grant ModemService implicit broadcast
    Change-Id: I590c198e12299e61748051338948e2b62c19b06b
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- device-common.mk
+++ device-common.mk
@@ -96,3 +96,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # NFC/camera interaction workaround - DO NOT COPY TO NEW DEVICES
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.notify_nfc=1
+
+# ModemService
+PRODUCT_COPY_FILES += \
+  device/google/muskie/modemservice.xml:system/etc/sysconfig/modemservice.xml
