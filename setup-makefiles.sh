LineageOS/android_device_google_muskie

Initial Proprietary files & extract
TODO: Fix ims

Change-Id: Ieb716f7bd32fd2f9ebe49b5001c6f6f8e999aab7

setup-makefiles.sh
@@ -1,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=walleye
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Write custom header
+function write_walleye_headers() {
+    write_header "$ANDROIDMK"
+
+    cat << EOF >> "$ANDROIDMK"
+LOCAL_PATH := \$(call my-dir)
+EOF
+    cat << EOF >> "$ANDROIDMK"
+ifneq (\$(filter walleye,\$(TARGET_DEVICE)),)
+EOF
+
+    write_header "$BOARDMK"
+    write_header "$PRODUCTMK"
+}
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_walleye_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/lineage-proprietary-files.txt
+
+# Done
+write_footers

Stupid-Jerk-Face/device_google_walleye

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

setup-makefiles.sh
@@ -1,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=walleye
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+SIX_ROOT="$MY_DIR"/../../..
+
+HELPER="$SIX_ROOT"/vendor/six/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Write custom header
+function write_walleye_headers() {
+    write_header "$ANDROIDMK"
+
+    cat << EOF >> "$ANDROIDMK"
+LOCAL_PATH := \$(call my-dir)
+EOF
+    cat << EOF >> "$ANDROIDMK"
+ifneq (\$(filter walleye,\$(TARGET_DEVICE)),)
+EOF
+
+    write_header "$BOARDMK"
+    write_header "$PRODUCTMK"
+}
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$SIX_ROOT"
+
+# Copyright headers and guards
+write_walleye_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/lineage-proprietary-files.txt
+
+# Done
+write_footers